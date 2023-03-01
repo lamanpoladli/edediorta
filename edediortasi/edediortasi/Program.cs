0e4dbffa808cbeff162e1ad994bd4aaa143e1427
lamanpoladli <lamanpoladli@gmail.com>
2023-03-01 21:07:56 +0400

    aa

---
+++ edediortasi/edediortasi/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] a = { 7, 6, 9, 8 };
+float say = 0;
+int cem = 0;
+for(int i=0; i<a.Length; i++)
+{
+    say++;
+    cem += a[i];
+}
+Console.WriteLine(cem / say);
---
+++ script.js
@@ -0,0 +1,10 @@
+let a = [ 7, 6, 9, 8 ];
+let say = 0;
+let cem = 0;
+for(let i=0; i<a.length; i++)
+{
+    say++;
+    cem += a[i];
+}
+console.log(cem / say);
+
